5e5481e87541147dbecdf433126c3c8d46bc5f5c
Rhema Linder <rhemalinder@gmail.com>
2015-01-24 23:18:23 -0600

    Added live mode. Made minimum size for poll to show webm file.

--- saveportrait/poll.php
+++ saveportrait/poll.php
@@ -6,7 +6,10 @@ $videos_folder = "uploads/";
 
 $files = array();
 foreach (glob($videos_folder."*webm") as $file) {
+	// echo $file;
   $ts = (int) explode(".", explode("-", $file)[1])[0];
+  if(filesize($file) < 1000000)
+    continue;
   $files[] =  array('file' => $file, 'timestamp'=>$ts);
 }
 
